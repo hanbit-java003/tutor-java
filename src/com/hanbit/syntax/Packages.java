d95e2c67e853cb18da530fb92ca11a17a1caee3b
1027 <1027@hb1027>
2017-06-02 12:24:54 +0900

    문법 설명 추가 3

---
+++ src/com/hanbit/syntax/Packages.java
@@ -0,0 +1,20 @@
+// 1. package
+// 클래스를 구분지어 놓기 위한 공간
+// Namespace
+// 보통 단체 또는 회사의 도메인 주소를 사용
+// 예: com.naver.news
+//     com.facebook.api
+package com.hanbit.syntax;
+
+// 2. import
+// 현재 클래스의 패키지와 다른 패키지의 클래스를 사용하기 위해서 선언
+// 단, java.lang 패키지의 클래스는 import 하지 않고 사용 가능
+//     (System, Byte, Integer, Long, Object, ...)
+import java.util.ArrayList;
+import java.util.List;
+
+public class Packages {
+
+	List list = new ArrayList();
+	
+}
